4f6ca512b092e57be22fc9eb83728e237cbeb047
mayapik <maya.pik@gmail.com>
2023-02-27 09:31:35 +0200

    3001

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 require("./config/passport");
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "http://localhost:3001", credentials: true }));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3001");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
