5e9ff825dc894bce18f6e48d435f8c75ce95b209
mayapik <maya.pik@gmail.com>
2023-02-27 09:52:05 +0200

    push

--- app.js
+++ app.js
@@ -14,11 +14,11 @@ app.use(
     secret: process.env.SECRET_KEY,
     saveUninitialized: true,
     resave: true,
-    cookie: {
-      maxAge: 30 * 24 * 60 * 60 * 1000,
-      sameSite: "none",
-      secure: true,
-    },
+    // cookie: {
+    //   maxAge: 30 * 24 * 60 * 60 * 1000,
+    //   sameSite: "none",
+    //   secure: true,
+    // },
   })
 );
 
