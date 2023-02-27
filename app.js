9bac11f92c61bc267c6392ba1fe30f1dbe1c7e70
mayapik <maya.pik@gmail.com>
2023-02-27 13:05:08 +0200

    push

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const session = require("express-session");
 const passport = require("passport");
 const routes = require("./routes");
 const app = express();
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
 
 app.use(
   session({
@@ -17,12 +15,7 @@ app.use(
     saveUninitialized: true,
     resave: true,
     cookie: {
-      domain: ".netlify.app",
       maxAge: 30 * 24 * 60 * 60 * 1000,
-      sameSite: "none",
-      secure: true,
-      httpOnly: false,
-      path: "/",
     },
   })
 );
@@ -32,7 +25,7 @@ require("./config/passport");
 
 app.use(
   cors({
-    origin: "https://incredible-sable-62c82c.netlify.app",
+    origin: "https://front-final-project.herokuapp.com",
     credentials: true,
   })
 );
@@ -53,11 +46,12 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(routes);
+
 app.use((req, res, next) => {
   console.log(req.session);
   console.log(req.user);
   next();
 });
-app.use(routes);
 
 app.listen(process.env.PORT);
