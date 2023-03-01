eca33808ce6ecba7dcfabd370f4a708acfaba48d
mayapik <maya.pik@gmail.com>
2023-03-01 20:41:59 +0200

    check

--- config/passport.js
+++ config/passport.js
@@ -8,92 +8,92 @@ const knex = require("knex")({
   connection: process.env.DATABASE_URL,
 });
 
-// const options = {
-//   usernameField: "username",
-//   passwordField: "password",
-// };
-
-// passport.use(
-//   new LocalStrategy(options, async (username, password, done) => {
-//     const user = await knex("guide").where({ username: username }).first();
-//     if (!user) {
-//       return done(null, false);
-//     }
-
-//     const match = await bcrypt.compare(password, user.password);
-//     if (match) {
-//       return done(null, user);
-//     } else {
-//       return done(null, false);
-//     }
-//   })
-// );
+const options = {
+  usernameField: "username",
+  passwordField: "password",
+};
 
 passport.use(
-  new LocalStrategy(
-    {
-      usernameField: "username",
-      passwordField: "password",
-      passReqToCallback: true,
-    },
-    async (req, username, password, done) => {
-      const userType = req.query.userType;
-      const user = await knex(userType).where({ username: username }).first();
-      if (!user) {
-        return done(null, false);
-      }
-      const match = await bcrypt.compare(password, user.password);
-      if (match) {
-        return done(null, user);
-      } else {
-        return done(null, false);
-      }
+  new LocalStrategy(options, async (username, password, done) => {
+    const user = await knex("guide").where({ username: username }).first();
+    if (!user) {
+      return done(null, false);
     }
-  )
+
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  })
 );
 
-passport.serializeUser((user, done) => {
-  done(null, user.guideid);
-});
-
-passport.deserializeUser((guideid, done) => {
-  console.log("sttart");
-  knex("guide")
-    .where({ guideid: guideid })
-    .first()
-    .then((user) => {
-      console.log(user);
-      done(null, user);
-    })
-    .catch((err) => {
-      console.log(err);
-      done(err);
-    });
-});
-
-// passport.serializeUser((user, done) => {
-//   console.log(user.user_id);
-//   done(null, user.user_id);
-// });
-// passport.deserializeUser((id, done) => {
-//   console.log("2" + id);
-//   Promise.all([
-//     knex("admin").where({ user_id: id }).select(),
-//     knex("child").where({ user_id: id }).select(),
-//     knex("guide").where({ user_id: id }).select(),
-//   ])
-//     .then(([admin, child, guide]) => {
-//       const user = admin[0] || child[0] || guide[0];
+// passport.use(
+//   new LocalStrategy(
+//     {
+//       usernameField: "username",
+//       passwordField: "password",
+//       passReqToCallback: true,
+//     },
+//     async (req, username, password, done) => {
+//       const userType = req.query.userType;
+//       const user = await knex(userType).where({ username: username }).first();
 //       if (!user) {
-//         console.log("deserializeUser user:", error);
 //         return done(null, false);
 //       }
-//       console.log("deserializeUser user:", user);
+//       const match = await bcrypt.compare(password, user.password);
+//       if (match) {
+//         return done(null, user);
+//       } else {
+//         return done(null, false);
+//       }
+//     }
+//   )
+// );
+
+// passport.serializeUser((user, done) => {
+//   done(null, user.guideid);
+// });
+
+// passport.deserializeUser((guideid, done) => {
+//   console.log("sttart");
+//   knex("guide")
+//     .where({ guideid: guideid })
+//     .first()
+//     .then((user) => {
+//       console.log(user);
 //       done(null, user);
 //     })
 //     .catch((err) => {
+//       console.log(err);
 //       done(err);
 //     });
 // });
+
+passport.serializeUser((user, done) => {
+  console.log(user.user_id);
+  done(null, user.user_id);
+});
+passport.deserializeUser((id, done) => {
+  console.log("2" + id);
+  Promise.all([
+    knex("admin").where({ user_id: id }).select(),
+    knex("child").where({ user_id: id }).select(),
+    knex("guide").where({ user_id: id }).select(),
+  ])
+    .then(([admin, child, guide]) => {
+      const user = admin[0] || child[0] || guide[0];
+      if (!user) {
+        console.log("deserializeUser user:", error);
+        return done(null, false);
+      }
+      console.log("deserializeUser user:", user);
+      done(null, user);
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
 
 module.exports = passport;
