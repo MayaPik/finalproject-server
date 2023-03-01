1d6c0f154cbc45363bf35dfd0048c710aa98e62f
mayapik <maya.pik@gmail.com>
2023-03-01 21:01:12 +0200

    arrrgg

--- app.js
+++ app.js
@@ -14,20 +14,20 @@ app.use(
   cors({ origin: "https://welcome.pickinguptime.com", credentials: true })
 );
 
-app.use(function (req, res, next) {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://welcome.pickinguptime.com"
-  );
-  res.header("Access-Control-Allow-Credentials", true);
+// app.use(function (req, res, next) {
+//   res.header(
+//     "Access-Control-Allow-Origin",
+//     "https://welcome.pickinguptime.com"
+//   );
+//   res.header("Access-Control-Allow-Credentials", true);
 
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  next();
-});
+//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+//   );
+//   next();
+// });
 
 app.use(cookieParser(process.env.SECRET_KEY));
 
