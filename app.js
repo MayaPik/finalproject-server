9b90504bdaceb298b99ee1f90ccc41a303a06286
mayapik <maya.pik@gmail.com>
2023-03-07 09:25:19 +0200

    push

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(
     saveUninitialized: false,
     resave: false,
     cookie: {
-      maxAge: 30 * 24 * 60 * 60 * 1000,
+      maxAge: 30 * 24 * 60 * 60 * 4000, //120 days
       secure: true,
       domain: ".pickinguptime.com",
     },
